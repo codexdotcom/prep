e737ab7fa2810f6e9a0b39466dc3eb502045b48c
successthecodess <successthecodess@gmail.com>
2026-06-02 18:30:07 -0700

    final push?

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/generated/prisma";
 
 const globalForPrisma = globalThis as unknown as { prisma: any };
-
 export const db = globalForPrisma.prisma ?? new PrismaClient();
-
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
